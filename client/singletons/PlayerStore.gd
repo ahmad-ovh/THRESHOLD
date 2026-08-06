14b28adc1203949527efb069119028274693549b
ahmad-ovh <ahmadseliaman@gmail.com>
2026-08-07 07:33:26 +0800

    feat: add customization state and persistence in PlayerStore

--- client/singletons/PlayerStore.gd
+++ client/singletons/PlayerStore.gd
@@ -2,6 +2,7 @@
 extends Node
 
 signal player_data_updated
+signal customization_updated
 
 var player_id: String = "player_01"
 var level: int = 1
@@ -9,9 +10,64 @@ var xp_progress: float = 0.0
 var daily_streak: int = 0
 var skill_vector: Dictionary = {"clarity": 0.5, "empathy": 0.5, "politeness": 0.5, "expression": 0.5}
 
+var customization: Dictionary = {
+	"skin_color": Color(0.92, 0.76, 0.65),
+	"hair_style": 0,
+	"hair_color": Color(0.24, 0.16, 0.10),
+	"eye_style": 1,
+	"eye_sclera_color": Color(1.0, 1.0, 1.0),
+	"eye_pupil_color": Color(0.1, 0.1, 0.1),
+	"eye_iris_color": Color(0.18, 0.55, 0.85),
+	"nose_style": 1,
+	"mouth_style": 1,
+	"accessory_style": 0
+}
+
+const CUSTOMIZATION_SAVE_PATH: String = "user://customization.json"
+
+func _ready() -> void:
+	load_customization()
+
 func update_from_status(data: Dictionary) -> void:
 	level = data.get("level", level)
 	xp_progress = data.get("xp_progress", xp_progress)
 	daily_streak = data.get("daily_streak", daily_streak)
 	skill_vector = data.get("skill_vector", skill_vector)
 	player_data_updated.emit()
+
+func save_customization() -> void:
+	var dict_to_save: Dictionary = {}
+	for k in customization.keys():
+		var val = customization[k]
+		if val is Color:
+			dict_to_save[k] = val.to_html(true)
+		else:
+			dict_to_save[k] = val
+	var file = FileAccess.open(CUSTOMIZATION_SAVE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_string(JSON.stringify(dict_to_save, "\t"))
+		file.close()
+
+func load_customization() -> void:
+	if not FileAccess.file_exists(CUSTOMIZATION_SAVE_PATH):
+		return
+	var file = FileAccess.open(CUSTOMIZATION_SAVE_PATH, FileAccess.READ)
+	if not file:
+		return
+	var json_string = file.get_as_text()
+	file.close()
+	var json = JSON.new()
+	if json.parse(json_string) == OK:
+		var data = json.get_data()
+		if data is Dictionary:
+			for k in data.keys():
+				if k in customization:
+					var default_val = customization[k]
+					if default_val is Color and data[k] is String:
+						customization[k] = Color.html(data[k])
+					elif default_val is int and data[k] is float:
+						customization[k] = int(data[k])
+					else:
+						customization[k] = data[k]
+	customization_updated.emit()
+
